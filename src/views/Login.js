5fea8a028a537fe79e8703e8a13803edfcc89956
heyyytamvo <tamvoforwork@gmail.com>
2023-10-12 17:22:14 +0700

    Modify Login and App

---
+++ src/views/Login.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+const Login = ( {LogInSuccess} ) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = async (e) => {
+  e.preventDefault();
+
+  try {
+    const response = await fetch("http://localhost:5000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ 'userName': username, 'password': password }),
+    });
+
+    // Handle the response
+    // ...
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
